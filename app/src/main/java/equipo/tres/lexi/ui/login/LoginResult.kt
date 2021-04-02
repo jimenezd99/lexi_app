651fd62e84626cd061e010428975c646fb940f4e
jimenezd99 <diana.jimenezn99@gmail.com>
2021-04-01 18:15:12 -0700

    Creación del cascarón

---
+++ app/src/main/java/equipo/tres/lexi/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package equipo.tres.lexi.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
