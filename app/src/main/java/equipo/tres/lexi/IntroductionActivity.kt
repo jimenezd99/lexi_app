9ec152e82a90e163993ce7c964eb530f59d5fab4
jimenezd99 <diana.jimenezn99@gmail.com>
2021-04-12 19:49:23 -0700

    Navegación rara

---
+++ app/src/main/java/equipo/tres/lexi/IntroductionActivity.kt
@@ -0,0 +1,4 @@
+package equipo.tres.lexi
+
+class IntroductionActivity {
+}
--- app/src/main/java/equipo/tres/lexi/NivelesActivity.kt
+++ app/src/main/java/equipo/tres/lexi/NivelesActivity.kt
@@ -1,10 +1,18 @@
 package equipo.tres.lexi
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
 
 class NivelesActivity : AppCompatActivity() {
+
+    var adapter: NivelAdapter? = null
+    var niveles = ArrayList<Nivel>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registro)
@@ -13,6 +21,63 @@ class NivelesActivity : AppCompatActivity() {
 
         btn_back.setOnClickListener {
             super.onBackPressed()
+        }
+        //homeViewModel = ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        val gvNiveles: GridView = findViewById(R.id.gv_niveles)
+
+        cargarNiveles()
+        adapter = NivelAdapter(this, niveles)
+        gvNiveles.adapter = adapter
+    }
+    private fun cargarNiveles(){
+        
+    }
+
+    class NivelAdapter : BaseAdapter {
+        var niveles = ArrayList<Nivel>()
+        var contexto: Context? = null
+
+        constructor(context: Context, niveles: ArrayList<Nivel>) {
+            this.contexto = context
+            this.niveles = niveles
+        }
+
+        override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+            var nivel = niveles[position]
+            var inflator =
+                contexto!!.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+            var vista = inflator.inflate(R.layout.cursos, null)
+
+            var imagen = vista.findViewById(R.id.iv_imagenNivel) as ImageView
+            var nombre = vista.findViewById(R.id.tv_nivel) as TextView
+
+            imagen.setImageResource(nivel.image)
+            nombre.setText(nivel.nombre)
+
+
+            imagen.setOnClickListener() {
+                var intent = Intent(contexto, IntroductionActivity::class.java)
+                intent.putExtra("nombre", nivel.nombre)
+                intent.putExtra("imagen", nivel.image)
+                intent.putExtra("introduccion", (nivel.introduccion))
+                intent.putExtra("nivel", nivel.nivel)
+                intent.putExtra("progreso", nivel.progreso)
+                contexto!!.startActivity(intent)
+            }
+            return vista
+        }
+
+        override fun getItem(position: Int): Any {
+            return niveles[position]
+        }
+
+        override fun getItemId(position: Int): Long {
+            return position.toLong()
+        }
+
+        override fun getCount(): Int {
+            return niveles.size
         }
     }
 }
