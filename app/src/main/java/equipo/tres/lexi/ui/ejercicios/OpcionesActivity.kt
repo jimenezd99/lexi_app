bbe303e5476f50157a5fa21744d04e276f360c1f
Angel Armando Aviles Martinez <avilesm.angel@gmail.com>
2021-05-19 14:04:22 -0700

    Agregados pantallas de ejercicios

--- app/src/main/java/equipo/tres/lexi/ui/ejercicios/OpcionesActivity.kt
+++ app/src/main/java/equipo/tres/lexi/ui/ejercicios/OpcionesActivity.kt
@@ -1,29 +1,27 @@
-package equipo.tres.lexi.ui.home
+package equipo.tres.lexi.ui.ejercicios
 
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
-import equipo.tres.lexi.ui.cursos.LeccionAvanzadaActivity
 import equipo.tres.lexi.R
-import equipo.tres.lexi.ui.ejercicios.TraducirActivity
+import equipo.tres.lexi.ui.home.NivelesActivity
 
-class IntroductionActivity: AppCompatActivity() {
-
+class OpcionesActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+        setContentView(R.layout.activity_opciones)
 
         val btn_back = findViewById(R.id.btn_back) as Button
+        val btn_nivel= findViewById(R.id.btn_continuar) as Button
 
         btn_back.setOnClickListener {
             super.onBackPressed()
         }
-        val btn_nivel= findViewById(R.id.btn_continuar) as Button
+
         btn_nivel.setOnClickListener(){
-            var intent = Intent(this, TraducirActivity::class.java)
+            var intent = Intent(this, CompletarActivity::class.java)
             this!!.startActivity(intent)
         }
-
     }
 }
